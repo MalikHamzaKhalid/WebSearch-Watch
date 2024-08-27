8dd77318257daf816bd79113fcfb4080ed2022c3
Sheraz Hussain <sherazh@joblogic.com>
2024-08-27 22:28:31 +0500

    Initial Commit

---
+++ WatchOS-biltz Watch App/WatchOS_biltzApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOS_biltzApp.swift
+//  WatchOS-biltz Watch App
+//
+//  Created by Sheraz Hussain on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_biltz_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
